c46eb24e877fc04aedfe52093b8586fc074c1ce0
Philip Ashton <philligan1@gmail.com>
2024-10-27 17:54:03 -0500

    input id's added to index.html and client.js.  total/monthly salary math added

--- client.js
+++ client.js
@@ -3,8 +3,24 @@ console.log ('check check 1-2');
 function submitForm(event) {
     console.log('submitForm');
     // Stop the page from refreshing
-    event.preventDefault();
+   
 
-    let firstNameVal = document.querySelector('#firstName-input').value;
-    console.log(firstNameVal);
+    let firstName= document.querySelector('#firstNameInput');
+    console.log(firstName);
+    let lastName= document.querySelector('#lastNameInput');
+    let idNumber= document.querySelector('#idNumberInput');
+    let title= document.querySelector('#titleInput');
+    let annualSalary= document.querySelector('#annualSalaryInput');
+
+
+
+let totalSalary = 0;
+    let salarySum = totalSalary += annualSalary;
+    for (let i = 0; i < totalSalary.length; i++) {
+        totalSalary += annualSalary[i];
+    }
+    console.log(salarySum);
+
+    let monthlySalary = (salarySum / 12);
+    console.log (monthlySalary);
 }
